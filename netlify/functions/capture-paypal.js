d3644958d64819bba3133eeaed098ae68c4e92a3
GLusetti13 <lghjiseppu@gmail.com>
2026-07-17 02:39:39 +0200

    Update capture-paypal.js

--- netlify/functions/capture-paypal.js
+++ netlify/functions/capture-paypal.js
@@ -1,5 +1,5 @@
 const PAYPAL_CLIENT_ID = 'AcLThrRvV8jviz-oARQgPOYnAm6ZkJ8cQ4Hz9q2TPZ7hnww3tzAfHHixgHDzKhxwndntm7W0FFEG9DRx';
-const PAYPAL_API = 'api-m.sandbox.paypal.com';
+const PAYPAL_API = 'https://api-m.sandbox.paypal.com';
 
 async function getAccessToken() {
   const credentials = Buffer.from(`${PAYPAL_CLIENT_ID}:${process.env.PAYPAL_SECRET_KEY}`).toString('base64');
